f9b5a35f661b60e74b54a844c8e8b1fe84ddb544
Pedrinho <ssixsanti@gmail.com>
2024-05-20 22:10:41 -0300

    First Commit

---
+++ app/src/main/java/com/example/ecommerce/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.ecommerce;
+
+import static androidx.core.content.ContextCompat.startActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
