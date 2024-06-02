2120b4ae0a87ccd6aaa5a677fdb4266f3a025079
Pedrinho <ssixsanti@gmail.com>
2024-06-02 15:29:00 -0300

    Fifth Commit

--- app/src/main/java/com/example/ecommerce/model/Produto.java
+++ app/src/main/java/com/example/ecommerce/model/Produto.java
@@ -1,4 +1,42 @@
 package com.example.ecommerce.model;
 
 public class Produto {
+
+    int imgproduct;
+    String descprod;
+    String nomeprod;
+    String priceprod;
+
+    public int getImgproduct() {
+        return imgproduct;
+    }
+
+    public void setImgproduct(int imgproduct) {
+        this.imgproduct = imgproduct;
+    }
+
+    public String getDescprod() {
+        return descprod;
+    }
+
+    public void setDescprod(String descprod) {
+        this.descprod = descprod;
+    }
+
+    public String getNomeprod() {
+        return nomeprod;
+    }
+
+    public void setNomeprod(String nomeprod) {
+        this.nomeprod = nomeprod;
+    }
+
+    public String getPriceprod() {
+        return priceprod;
+    }
+
+    public void setPriceprod(String priceprod) {
+        this.priceprod = priceprod;
+    }
 }
+
